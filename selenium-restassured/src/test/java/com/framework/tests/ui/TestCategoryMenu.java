5aed9f7bed1f6f2b088c7330e0fedeb37b8ae28c
Shabbir Rayeen <sawone2893@gmail.com>
2025-12-26 00:14:21 +0530

    Added sample test.

---
+++ selenium-restassured/src/test/java/com/framework/tests/ui/TestCategoryMenu.java
@@ -0,0 +1,18 @@
+package com.framework.tests.ui;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.framework.base.BaseTest;
+import com.framework.pages.HomePage;
+
+public class TestCategoryMenu extends BaseTest {
+	//https://www.youtube.com/watch?v=8YxJv4l-LOo&list=PLFGoYjJG_fqpc4jcqbeXqMIT3-60nfLr6&index=5
+	@Test
+	public void testCategoryMenu() {
+		HomePage hp = new HomePage();
+		hp.selectItemFromCategoryMenu("Makeup", "Cheeks");
+		Assert.assertEquals(hp.getCurrentPageTitle(), "Cheeks", "Page title mismatch");
+	}
+
+}
